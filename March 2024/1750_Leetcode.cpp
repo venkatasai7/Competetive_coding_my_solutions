e2700ee469e04fa045b03ddff080ba7644ed658b
Venkata Sai Kuniganti <87575630+venkatasai7@users.noreply.github.com>
2024-03-05 15:43:42 +0000

    March 2024

---
+++ March 2024/1750_Leetcode.cpp
@@ -0,0 +1,25 @@
+//1750. Minimum Length of String After Deleting Similar Ends
+ 
+// https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends/?envType=daily-question&envId=2024-03-05
+
+class Solution {
+public:
+    int minimumLength(string s) {
+
+        int l =0,r = s.length()-1;
+        //cout<<l<<" "<<r<<endl;
+        while( l<r && s[l]==s[r])
+        {
+           // cout<<l<<" "<<r<<endl;
+            char ch = s[l];
+            while(l<=r && s[l]==ch)
+                l++;
+            while(l<=r && s[r]==ch)
+                r--;
+        }
+
+        //cout<<"\nFinal : "<<l<<" "<<r<<endl;
+        
+        return (r-l+1 >=0) ? r-l+1 : 0;
+    }
+};
